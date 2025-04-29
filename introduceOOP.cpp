23aca7f9ba2742c79c7a1379ada88a6375674bd4
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-04-29 15:19:51 +0700

    Menambahkan variabel  introduce OOP_2

--- introduceOOP.cpp
+++ introduceOOP.cpp
@@ -2,20 +2,12 @@
 using namespace std;
 
 class Mahasiswa {
-    public :
+public :
     int nim;
     string nama;
     float nilai;
-    void printData() {
-        cout << " NIM \t = "<< nim <<endl;
-        cout << " Nama \t = " << nama <<endl;
-        cout << " Nilai \t = " << nilai <<endl;
-    }
-};
 
+};
 int main () {
-    Mahasiswa.mhs;
-    mhs.nim;
-    mhs.nama;
-    mhs.nilai;
+
 }
