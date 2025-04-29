b37d995a440dbdfd762ba8cb4f15b7bf30e76ba4
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-04-29 15:42:18 +0700

    first commit

--- introduceOOP.cpp
+++ introduceOOP.cpp
@@ -1,24 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
-public :
-    int nim;
-    string nama;
-    float nilai;
-
-    void printData() {
-        cout << " NIM : " << nim << endl;
-        cout << " Nama : " << nama << endl;
-        cout << " Nilai : " << nilai << endl;
-    }
-
-};
-int main () {
-   Mahasiswa mhs;
-   mhs.nim = 2024;
-   mhs.nama = "Faiz";
-   mhs.nilai = 90.5;
-
-   mhs.printData();
-}
